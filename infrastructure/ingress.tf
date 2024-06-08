1ddde711e4e2788918ec0da70848b521da05114c
jblackowiak <jblackowiak@cvent.com>
2024-06-08 10:57:39 -0400

    iterative change

--- infrastructure/ingress.tf
+++ infrastructure/ingress.tf
@@ -28,6 +28,42 @@
 #   }
 # }
 
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                 = data.aws_eks_cluster_auth.cluster.token
+# }
+
+# resource "kubernetes_deployment" "my_app" {
+#   metadata {
+#     name = "my-app"
+#   }
+#   spec {
+#     replicas = 2
+#     selector {
+#       match_labels = {
+#         app = "my-app"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           app = "my-app"
+#         }
+#       }
+#       spec {
+#         container {
+#           image = "851725465050.dkr.ecr.us-east-1.amazonaws.com/hello-repository:latest"  # replace with your Docker image
+#           name  = "my-app"
+#           port {
+#             container_port = 3000
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+
 # resource "kubernetes_namespace" "aws_lb_controller" {
 #   metadata {
 #     name = "kube-system"
@@ -55,4 +91,59 @@
 #     name  = "serviceAccount.name"
 #     value = "aws-load-balancer-controller"
 #   }
+# }
+
+# resource "kubernetes_service" "example" {
+#   metadata {
+#     name = "example"
+#   }
+#   spec {
+#     selector = {
+#       App = kubernetes_deployment.my_app.metadata[0].labels.App
+#     }
+#     port {
+#       port        = 443
+#       target_port = 3000
+#     }
+#     type = "LoadBalancer"
+#   }
+# }
+
+# resource "aws_route53_zone" "main" {
+#   name = "example.com"
+# }
+
+# resource "aws_acm_certificate" "cert" {
+#   domain_name       = "example.com"
+#   validation_method = "DNS"
+# }
+
+# resource "aws_route53_record" "validation" {
+#   name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+#   type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+#   zone_id = "${aws_route53_zone.main.zone_id}"
+#   records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+#   ttl     = 60
+# }
+
+# resource "aws_acm_certificate_validation" "cert" {
+#   certificate_arn         = "${aws_acm_certificate.cert.arn}"
+#   validation_record_fqdns = ["${aws_route53_record.validation.fqdn}"]
+# }
+
+# data "aws_lb" "my_app" {
+#   name = "my-app-load-balancer-name" # replace with your load balancer name
+# }
+
+# resource "aws_route53_record" "www" {
+#   name    = "www.example.com"
+#   type    = "A"
+
+#   alias {
+#     name                   = data.aws_lb.my_app.dns_name
+#     zone_id                = data.aws_lb.my_app.zone_id
+#     evaluate_target_health = true
+#   }
+
+#   zone_id = aws_route53_zone.main.zone_id
 # }
