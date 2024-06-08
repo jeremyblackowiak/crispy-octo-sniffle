b4a4f139df47df7b222a3cbf518d19898f787172
jblackowiak <jblackowiak@cvent.com>
2024-06-08 08:43:10 -0400

    more boilerplate

---
+++ infrastructure/ingress.tf
@@ -0,0 +1,9 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {}
+
+// roles for externalDNS
+// roles for AWS Load Balancer Controller
+
+// user for externalDNS
