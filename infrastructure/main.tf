814741182e2ce06efd3961c22b5ad55af3c69fa4
jblackowiak <jblackowiak@cvent.com>
2024-06-09 12:23:24 -0400

    through load balancer controller

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,7 +8,7 @@ provider "aws" {}
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "test-eks-${random_string.suffix.result}"
 }
 
 locals {
@@ -30,16 +30,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "main"
+  cidr = "10.0.0.0/16"
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
-  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
+  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
@@ -51,67 +50,11 @@ module "vpc" {
     "type" = "internal"
   }
 
-  intra_subnet_tags = {
-    "type" = "intra"
+  tags = {
+    Environment = "test"
   }
-
-  tags = local.tags
 }
 
-resource "aws_iam_role" "eks-cluster" {
-  name = "eks-cluster-${local.cluster_name}"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-cluster.name
-}
-
-resource "aws_iam_role" "example" {
-  name = "eks-fargate-profile-example"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks-fargate-pods.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSFargatePodExecutionRolePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-  role       = aws_iam_role.example.name
-}
-
-# resource fargate_profile "example" {
-#   cluster_name = module.eks.cluster_id
-#   fargate_profile_name = "example"
-#   pod_execution_role_arn = aws_iam_role.example.arn
-#   subnet_ids = module.vpc.private_subnets
-#   namespace = "default"
-#   selectors {
-#     namespace = "default"
-#   }
-# }
 
 data "aws_subnets" "private" {
   filter {
@@ -124,151 +67,74 @@ data "aws_subnets" "private" {
   }
 }
 
-data "aws_subnets" "intra" {
-  filter {
-    name = "vpc-id"
-    values = [module.vpc.vpc_id]
-  }
+# data "aws_subnets" "intra" {
+#   filter {
+#     name = "vpc-id"
+#     values = [module.vpc.vpc_id]
+#   }
 
-  tags = {
-    "type" = "intra"
+#   tags = {
+#     "type" = "intra"
+#   }
+# }
+
+resource "aws_security_group" "eks" {
+    name        = "eks sg"
+    description = "Allow traffic"
+    vpc_id      = module.vpc.vpc_id
+
+    ingress {
+      description      = "everything for test"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+      "kubernetes.io/cluster/${local.cluster_name}": "owned"
+    }
   }
-}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
-  depends_on = [ module.vpc, data.aws_subnets.private, data.aws_subnets.intra ]
+  depends_on = [ module.vpc, data.aws_subnets.private ]
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access           = true
   cluster_endpoint_private_access = true
   enable_cluster_creator_admin_permissions = true
+  cluster_additional_security_group_ids = [aws_security_group.eks.id]
 
   enable_irsa = true
 
   eks_managed_node_group_defaults = {
-    disk_size = 20
+    disk_size = 30
   }
 
   eks_managed_node_groups = {
     nodes = {
-      min_size     = 1
-      max_size     = 1
       desired_size = 1
+      min_size     = 1
+      max_size     = 3
 
       instance_types = ["t3.micro"]
     }
   }
 
-  vpc_id     = module.vpc.vpc_id
-  # subnet_ids = ["subnet-047cfc38be1b76239", "subnet-0b58727fa8ad9ff47", "subnet-08dfff2c0a7400641"]
-  # control_plane_subnet_ids = ["subnet-05ef8cbb5d3603a3e", "subnet-05707e443a05c42f5", "subnet-05a74ebfc6655bd7d"]
-  subnet_ids = toset(data.aws_subnets.private.ids)
-  control_plane_subnet_ids = toset(data.aws_subnets.intra.ids)
-
-  # create_cluster_security_group = false
-  # create_node_security_group    = false
-
-  # fargate_profile_defaults = {
-  #   iam_role_additional_policies = {
-  #     additional = aws_iam_policy.additional.arn
-  #   }
-  # }
-
-  # fargate_profiles = {
-  #   example = {
-  #     name = "example"
-  #     selectors = [
-  #       {
-  #         namespace = "backend"
-  #         labels = {
-  #           Application = "backend"
-  #         }
-  #       },
-  #       {
-  #         namespace = "app-*"
-  #         labels = {
-  #           Application = "app-wildcard"
-  #         }
-  #       }
-  #     ]
-
-  #     # Using specific subnets instead of the subnets supplied for the cluster itself
-  #     # subnet_ids = [module.vpc.private_subnets[1]]
-  #     subnet_ids = [data.aws_subnets.private.ids[0]]
-
-  #     tags = {
-  #       Owner = "secondary"
-  #     }
-  #   }
-  #   kube-system = {
-  #     selectors = [
-  #       { namespace = "kube-system" }
-  #     ]
-  #   }
-  # }
-
-}
-  # eks_managed_node_group_defaults = {
-  #   ami_type = "AL2_x86_64"
-
-  # }
-
-  # eks_managed_node_groups = {
-  #   one = {
-  #     name = "node-group-1"
-
-  #     instance_types = ["t3.small"]
-
-  #     min_size     = 1
-  #     max_size     = 1
-  #     desired_size = 1
-  #   }
-
-  #   two = {
-  #     name = "node-group-2"
-
-  #     instance_types = ["t3.small"]
-
-  #     min_size     = 1
-  #     max_size     = 1
-  #     desired_size = 1
-  #   }
-  # }
-
-
-
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_iam_policy" "additional" {
-  name = "${local.cluster_name}-additional"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
