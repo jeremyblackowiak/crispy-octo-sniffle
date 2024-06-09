b9c5481e8832f365b38e497eea7839fac2e564d5
jblackowiak <jblackowiak@cvent.com>
2024-06-09 15:59:53 -0400

    iterative change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -128,8 +128,8 @@ module "eks" {
 
   eks_managed_node_groups = {
     nodes = {
-      desired_size = 1
-      min_size     = 1
+      desired_size = 3
+      min_size     = 3
       max_size     = 3
 
       instance_types = ["t3.micro"]
